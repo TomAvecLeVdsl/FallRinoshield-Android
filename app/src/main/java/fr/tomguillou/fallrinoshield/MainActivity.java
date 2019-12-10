1a5b2f847444f033edc619b980ad3964488a5ff5
Tom <tom.guillou10@gmail.com>
2019-12-10 19:01:32 +0100

    Updating APP

--- app/src/main/java/fr/tomguillou/fallrinoshield/MainActivity.java
+++ app/src/main/java/fr/tomguillou/fallrinoshield/MainActivity.java
@@ -2,6 +2,8 @@ package fr.tomguillou.fallrinoshield;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -10,125 +12,41 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.seismic.ShakeDetector;
 
 import java.text.DecimalFormat;
 import java.util.Collections;
 import java.util.LinkedList;
 
+import static android.view.Gravity.CENTER;
+import static android.widget.ListPopupWindow.MATCH_PARENT;
 import static java.lang.Character.SIZE;
 import static java.lang.Math.sqrt;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements ShakeDetector.Listener {
 
-    private SensorManager sensorManager;
-    Sensor accelerometer;
-
-    private long lastUpdate = -1;
-
-    private float accel_values[];
-    private float last_accel_values[];
-    LinkedList<Float> samples;
-    public static final String TAG = "MainActivity" ;
-    private final float THRESHOLD =  15f;
-    private final int DEFAULT_SIZE = 10;
-
-//    private int fallThreshold = 10;
-
-    private float mAccelCurrent = SensorManager.GRAVITY_EARTH;
-    private float mAccelLast = SensorManager.GRAVITY_EARTH;
-    private float mAccel = 0.00f;
-
-    private final static int CHECK_INTERVAL = 100;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        samples = new LinkedList<Float>();
 
-        Log.d(TAG,"onCreate : Initialising Sensor") ;
-        sensorManager =(SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.d(TAG,"onCreate : Accelerometer Registered") ;
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        ShakeDetector sd = new ShakeDetector((ShakeDetector.Listener) this);
+        sd.start(sensorManager);
+
+        TextView tv = new TextView(this);
+        tv.setGravity(CENTER);
+        tv.setText("Shake me, bro!");
+        setContentView(tv, new ActionBar.LayoutParams(MATCH_PARENT, MATCH_PARENT));
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        long curTime = System.currentTimeMillis();
-
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            // sampling frequency f= 10Hz.
-            if ((curTime - lastUpdate) > CHECK_INTERVAL) {
-
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-
-                accel_values = event.values.clone();
-
-                if (last_accel_values != null) {
-
-                    mAccelLast = mAccelCurrent;
-                    mAccelCurrent =(float)Math.sqrt(accel_values[0]* accel_values[0] + accel_values[1]*accel_values[1]
-                            + accel_values[2]*accel_values[2]);
-
-                    // Initial approach
-//                    float delta = mAccelCurrent - mAccelLast;
-//                    mAccel = mAccel * 0.9f + delta;
-
-
-
-                    add(mAccelCurrent);
-                    if (isFull() && isFallDetected()){
-                        Log.w(TAG, "Fall detected");
-
-                    }
-//                    Inital approach
-//                    =====================================
-//                    if (mAccel > fallThreshold) {
-//
-//                        Log.w(TAG, "acceleration greater than threshold");
-//                        // Send the value back to the Activity
-//                        msg = mHandler.obtainMessage(Constants.MESSAGE_EMERGENCY);
-//                        mHandler.sendMessage(msg);
-//                    }
-                }
-
-                last_accel_values = accel_values.clone();
-            }
-        }
-
+    public void hearShake() {
+        Toast.makeText(this, "Don't shake me, bro!", Toast.LENGTH_SHORT).show();
     }
 
-    public Boolean isFull(){
-        return (samples.size() > SIZE);
-    }
-    public Boolean isFallDetected(){
-        Float max = Collections.max(samples);
-        Float min = Collections.min(samples);
-        Float diff = Math.abs(max-min);
-
-        // check if min value detected earlier than max
-        Boolean isFall = ( samples.indexOf(max) > samples.indexOf(min) );
-
-        return (diff>THRESHOLD && isFall);
-    }
-
-    public void add(float value){
-        if (!isFull()){
-            samples.add(new Float(value));
-            //add value
-        } else {
-            samples.removeFirst();
-            samples.add(new Float(value));
-        }
-    }
-
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
